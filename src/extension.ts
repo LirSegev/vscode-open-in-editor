c0e46573b0097716730a5dc4cb6ca9c29a90b59a
Lir Segev <lir.segev@gmail.com>
2019-11-05 03:25:23 +0100

    Create server and listen to requests

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,18 @@
 import * as vscode from 'vscode';
+import * as http from 'http';
 
-export function activate(context: vscode.ExtensionContext) {}
+let server: http.Server | undefined = undefined;
+
+export function activate(context: vscode.ExtensionContext) {
+	server = http.createServer((req, res) => {
+	});
+
+	server.listen(vscode.workspace.getConfiguration('openInEditor').get('port'));
+}
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (server) {
+		server.close();
+	}
+}
